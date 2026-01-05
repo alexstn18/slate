e74b56c1060f280979644c11b4fbc54bfa6e03d4
alex s. <116825414+alexstn18@users.noreply.github.com>
2026-01-05 16:39:22 +0100

    add: typedefs for u32, etc in pch; DescriptorAllocator

--- include/pch.hpp
+++ include/pch.hpp
@@ -43,3 +43,14 @@ using ComPtr = Microsoft::WRL::ComPtr<T>;
 #define _64MB _MB(64)
 #define _128MB _MB(128)
 #define _256MB _MB(256)
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+typedef float    f32;
+typedef double   f64;
--- source/rendering/descriptor_allocator.cpp
+++ source/rendering/descriptor_allocator.cpp
@@ -2,3 +2,67 @@
 #include "rendering/descriptor_allocator.hpp"
 
 using namespace slate;
+
+DescriptorAllocator::DescriptorAllocator(D3D12_DESCRIPTOR_HEAP_TYPE type, u32 numDescriptorsPerHeap) :
+	m_Type{type},
+	m_NumDescriptorsPerHeap{numDescriptorsPerHeap}
+{
+}
+
+DescriptorAllocator::~DescriptorAllocator()
+{
+}
+
+DescriptorAllocation DescriptorAllocator::Allocate(u32 numDescriptors)
+{
+	std::lock_guard<std::mutex> lock(m_AllocationMutex);
+	DescriptorAllocation descAlloc{};
+
+	for(auto iter = m_AvailableHeaps.begin(); iter != m_AvailableHeaps.end(); ++iter)
+	{
+		auto allocatorPage = m_HeapPool[*iter];
+		descAlloc = allocatorPage->Allocate(numDescriptors);
+		if(allocatorPage->NumFreeHandles() == 0)
+		{
+			iter = m_AvailableHeaps.erase(iter);
+		}
+
+		if(!descAlloc.IsNull())
+		{
+			break;
+		}
+
+		if(descAlloc.IsNull())
+		{
+			m_NumDescriptorsPerHeap = std::max(m_NumDescriptorsPerHeap, numDescriptors);
+			auto newPage = CreateAllocatorPage();
+			descAlloc = newPage->Allocate(numDescriptors);
+		}
+	}
+
+	return descAlloc;
+}
+
+void DescriptorAllocator::ReleaseStaleDescriptors(u64 frameNumber)
+{
+	std::lock_guard<std::mutex> lock(m_AllocationMutex);
+
+	for(size_t i = 0; i < m_HeapPool.size(); ++i)
+	{
+		auto page = m_HeapPool[i];
+		page->ReleaseStaleDescriptors(frameNumber);
+		if(page->NumFreeHandles() > 0)
+		{
+			m_AvailableHeaps.insert(i);
+		}
+	}
+}
+
+std::shared_ptr<DescriptorAllocatorPage> DescriptorAllocator::CreateAllocatorPage()
+{
+	auto newPage = std::make_shared<DescriptorAllocatorPage>(m_Type, m_NumDescriptorsPerHeap);
+	m_HeapPool.emplace_back(newPage);
+	m_AvailableHeaps.insert(m_HeapPool.size() - 1);
+
+	return newPage;
+}
